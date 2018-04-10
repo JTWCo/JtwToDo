4a30bacbfe2281ace084afacd1dd4b98b6e6bd29
Jen Wolke <jwolke5@gmail.com>
2018-04-09 22:00:48 -0600

    Add about/modal dialogs

--- JtwToDo.UI/src/app/app.module.ts
+++ JtwToDo.UI/src/app/app.module.ts
@@ -3,15 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 
-
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
 import { ToDoComponent } from './to-do/to-do.component';
 import { ToDoListComponent } from './to-do-list/to-do-list.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
+    AboutComponent,
     ToDoComponent,
     ToDoListComponent
   ],
